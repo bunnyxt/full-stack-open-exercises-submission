38a843ed2b1a7809ef7e7ce1e9ee23b0bd9d161f
bunnyxt <bunnyxt@outlook.com>
2020-09-05 22:16:05 +0800

    Finish part8 b.

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import { gql, useQuery, useMutation } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    born
+    bookCount
+  }
+}
+`
+
+const UPDATE_AUTHOR_BIRTH_YEAR = gql`
+mutation updateAuthorBirthYear($name: String!, $setBornTo: Int!) {
+  editAuthor (
+    name: $name, 
+    setBornTo: $setBornTo
+  ) {
+    name
+    born
+  }
+}
+`
+
+const Authors = (props) => {
+  const [selectedAuthor, setSelectedAuthor] = useState('')
+  const [newBirthYear, setNewBirthYear] = useState('')
+  const [ updateAuthorBirthYear ] = useMutation(UPDATE_AUTHOR_BIRTH_YEAR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  const goUpdateAuthorBirthYear = () => {
+    updateAuthorBirthYear({ variables: { name: selectedAuthor, setBornTo: Number(newBirthYear) } })
+    setNewBirthYear('')
+  }
+  
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  const authors = result.data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <h2>Set birthyear</h2>
+      <select value={selectedAuthor} onChange={e => setSelectedAuthor(e.target.value)}>
+        {authors.map(a => 
+          <option key={a.name} value={a.name}>{a.name}</option>
+        )}
+      </select>
+      <div>
+        born<input type='number' value={newBirthYear} onChange={e => setNewBirthYear(e.target.value)} />
+      </div>
+      <button onClick={goUpdateAuthorBirthYear}>update author</button>
+    </div>
+  )
+}
+
+export default Authors
---
+++ part8/library-frontend/src/components/NewBook.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { gql, useMutation } from '@apollo/client'
+
+const CREATE_BOOK = gql`
+mutation createBook($title: String!, $published: Int!, $author: String!, $genres: [String!]!) {
+  addBook(
+    title: $title,
+    published: $published,
+    author: $author,
+    genres: $genres
+  ) {
+    title
+    published
+    author
+    genres
+  }
+}
+`
+
+const NewBook = (props) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuhtor] = useState('')
+  const [published, setPublished] = useState('')
+  const [genre, setGenre] = useState('')
+  const [genres, setGenres] = useState([])
+
+  const [ createBook ] = useMutation(CREATE_BOOK)
+
+  if (!props.show) {
+    return null
+  }
+
+  const submit = (event) => {
+    event.preventDefault()
+    
+    createBook({ variables: { title, author, published: Number(published), genres } })
+
+    setTitle('')
+    setPublished('')
+    setAuhtor('')
+    setGenres([])
+    setGenre('')
+  }
+
+  const addGenre = () => {
+    setGenres(genres.concat(genre))
+    setGenre('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          title
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            value={author}
+            onChange={({ target }) => setAuhtor(target.value)}
+          />
+        </div>
+        <div>
+          published
+          <input
+            type='number'
+            value={published}
+            onChange={({ target }) => setPublished(target.value)}
+          />
+        </div>
+        <div>
+          <input
+            value={genre}
+            onChange={({ target }) => setGenre(target.value)}
+          />
+          <button onClick={addGenre} type="button">add genre</button>
+        </div>
+        <div>
+          genres: {genres.join(' ')}
+        </div>
+        <button type='submit'>create book</button>
+      </form>
+    </div>
+  )
+}
+
+export default NewBook
